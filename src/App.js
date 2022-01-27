5686fae9419fbbe6d7405730e5a0d72ae74f92d2
limchaekyung <corud505@naver.com>
2022-01-27 23:49:05 +0900

    movie app 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import {
   BrowserRouter as Router,
-  Switch,
+  Routes,
   Route,
 } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
+import List from "./router/List";
+import Nav from "./router/Nav";
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from 'recoil';
 
 function App() {
   return(
-    <Router>
-      <Switch>
-        <Route path="/movie/:id">
-          <Detail />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <RecoilRoot>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/page/:detail/:num" element={<List />}/>
+          <Route path="/movie/:id" element={<Detail />} />
+          <Route path={ProgressEvent.env.PUBLIC_URL + "/"} element={<Home />} />
+        </Routes>
+      </Router>
+    </RecoilRoot>
   );
+  /* ProgressEvent.env.PUBLIC_URL : public의 절대경로  */
 }
 
 export default App;
---
+++ src/atom/Atom.js
@@ -0,0 +1,13 @@
+import {atom} from "recoil"
+
+const listPageReLoading = atom({
+    key: 'listPageReLoading', //
+    default: false,
+});
+
+const focusNav = atom({
+    key: 'focusNav',
+    default: "",
+})
+
+export {listPageReLoading, focusNav};
